dcdf13a3f5cfd1833a979eb70249a08ce5bdd188
khyunji <klxcxl@gmail.com>
2021-03-25 23:54:51 +0900

    feat: ✨ Senior Main 페이지 title 추가
    - vuetify와 scss 파일 간의 우선 순위 충돌 해결 필요

---
+++ frontend/src/components/senior/TitleBox.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="titleBox">
+      <h1 class="title">{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+import "@/components/css/senior/titleBox.scss";
+export default {
+  name: "titleBox",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  }
+};
+</script>
---
+++ frontend/src/views/senior/SeniorMain.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="seniorMain">
+      <TitleBox title="김싸피님, 나리를 불러서 원하는 기능을 실행하세요!"></TitleBox>
+  </div>
+</template>
+
+<script>
+import TitleBox from '@/components/senior/TitleBox.vue';
+export default {
+  name: "SeniorMain",
+  components: {
+    TitleBox,
+  },
+}
+</script>
