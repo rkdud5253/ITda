047afcca74c16a4b43990ce45e158028bf9052f8
khyunji <klxcxl@gmail.com>
2021-03-31 23:41:24 +0900

    refactor: 🐙 어르신 메인 화면 리팩토링
    - 어르신 메인 화면 flex 적용 및 코드 개선
    
    ✅ Closes: #S04P23A404-62

--- frontend/src/views/senior/SeniorMain.vue
+++ frontend/src/views/senior/SeniorMain.vue
@@ -1,26 +1,27 @@
 <template>
   <div class="seniorMain">
-    <TitleBox title="김싸피님, 나리를 불러서 원하는 기능을 실행하세요!"></TitleBox>
+    <TitleBox title="김싸피님, 나리를 불러서 원하는 기능을 실행하세요!" />
     <div class="menuContainer">
-      <DailyExercise />
-      <PhotoDiary />
-      <FamilyGame />
+      <DailyExerciseMenu />
+      <PhotoDiaryMenu />
+      <FamilyGameMenu />
     </div>
   </div>
 </template>
 
 <script>
+import '@/components/css/senior/seniorMain.scss';
 import TitleBox from '@/components/senior/TitleBox.vue';
-import DailyExercise from '@/components/senior/DailyExercise.vue';
-import PhotoDiary from '@/components/senior/PhotoDiary.vue';
-import FamilyGame from '@/components/senior/FamilyGame.vue';
+import DailyExerciseMenu from '@/components/senior/main/DailyExerciseMenu.vue';
+import PhotoDiaryMenu from '@/components/senior/main/PhotoDiaryMenu.vue';
+import FamilyGameMenu from '@/components/senior/main/FamilyGameMenu.vue';
 export default {
   name: "SeniorMain",
   components: {
     TitleBox,
-    DailyExercise,
-    PhotoDiary,
-    FamilyGame,
+    DailyExerciseMenu,
+    PhotoDiaryMenu,
+    FamilyGameMenu,
   },
 }
 </script>
