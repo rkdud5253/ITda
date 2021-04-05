96fbd66d4312f1bfad438d028bb4279f7ec31174
khyunji <klxcxl@gmail.com>
2021-04-06 02:53:38 +0900

    feat: ✨ 어르신 - 사진 일기장 loader 추가 및 안내 문구 수정

--- frontend/src/views/senior/PhotoDiary.vue
+++ frontend/src/views/senior/PhotoDiary.vue
@@ -1,26 +1,45 @@
 <template>
   <div class="photoDiary">
-    <TitleBox title="왼쪽 사진의 동작을 따라해보세요!"/>
+    <TitleBox title="왼쪽 사진의 동작을 따라한 뒤 웃어주세요!"/>
     <div class="photos">
       <ExampleImage />
       <SeniorPhoto />
     </div>
-    <p class="explain">나리가 “하나, 둘, 셋!”을 외치면 사진이 찍혀요.</p>
+    <p class="explain">{{ items[i] }}</p>
+    <!-- <p v-for="(ment, i) in items"
+      :key="i"
+      class="explain"
+    >{{ ment }}</p> -->
     <hr class="line">
+    <Loader />
   </div>
 </template>
 
 <script>
 import '@/components/css/senior/photoDiary.scss';
+import Loader from '@/components/senior/Loader.vue';
 import TitleBox from '@/components/senior/TitleBox.vue';
 import ExampleImage from '@/components/senior/photo_diary/ExampleImage.vue';
 import SeniorPhoto from '@/components/senior/photo_diary/SeniorPhoto.vue';
 export default {
   name: "SeniorMain",
   components: {
+    Loader,
     TitleBox,
     ExampleImage,
     SeniorPhoto,
   },
+  data() {
+    return {
+      i: 0,
+      items: [
+      "활짝 웃으면 사진이 찍혀요😄",
+      "카메라를 보고 웃어주세요😁"
+      ]
+    }
+  },
+  methods: {
+
+  }
 }
 </script>
--- frontend/src/views/senior/PhotoDiaryResult.vue
+++ frontend/src/views/senior/PhotoDiaryResult.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="photoDiaryResult">
-    <TitleBox title="사진이 잘 찍혔어요!" />
+    <TitleBox title="오늘의 사진이 잘 찍혔어요!" />
     <SeniorPhoto />
-    <p class="explain">다시 찍으시려면 “나리야”를 불러주세요.</p>
-    <hr class="line">
+    <p class="explain">사진이 마음에 드시면 "나리야 저장"</p>
+    <hr class="line1">
+    <p class="explain">다시 찍으시려면 “나리야 다시”</p>
+    <hr class="line2">
   </div>
 </template>
 
