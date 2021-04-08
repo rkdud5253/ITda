bcb01f85173247493498f64a1e0071d9d8286cc7
khyunji <klxcxl@gmail.com>
2021-04-08 17:51:31 +0900

    chore: 🧵 어르신 - 오늘의 체조 엔터 추가

--- frontend/src/views/senior/exercise/DailyExercise.vue
+++ frontend/src/views/senior/exercise/DailyExercise.vue
@@ -9,9 +9,9 @@
         >
         <div class="video">
           <h3 class="explain">
-            PoseNet을 이용한 어르신 실시간 영상이 들어갈 곳입니다.
-            실행을 위해서는 Raspberry Pi가 필요하므로,
-            발표 시 시연으로 대체하겠습니다.
+            이곳에는 PoseNet을 이용한<br> 어르신 실시간 영상이 들어갑니다.<br>
+            실행을 위해서는<br> Raspberry Pi가 필요하므로,<br>
+            발표 시 시연으로<br> 보여드릴 예정입니다.
           </h3>
         </div>
       </div>
